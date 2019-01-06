fe2706ae8a8960acb4840da9a663055458ba25e4
varun93 <1hvarun@gmail.com>
2019-01-05 22:48:16 -0600

    find first

--- Chapter1.scala
+++ Chapter1.scala
@@ -1,4 +1,4 @@
-object MyModule{
+object Chapter1{
     def abs(n : Int) : Int = {
         if(n < 0) -n
         else n
@@ -13,9 +13,30 @@ object MyModule{
         @annotation.tailrec
         def loop(n : Int, a : Int, b : Int) : Int = {
             if(n < 3) b
-            else loop(n-1, b, a+b)
+            else loop(n - 1, b, a + b)
         }
-        loop(n,0,1)
+        loop(n, 0, 1)
+    }
+
+
+    def findFirst(array : Array[Int], key : Int) : Int = {
+        @annotation.tailrec
+        def loop(index : Int) : Int = {
+            if(index >= array.length) -1
+            else if(array(index) == key) index
+            else loop(index + 1)
+        }
+        loop(0)
+    }
+
+    def findFirstHof(array : Array[A], f : A => Boolean) : Int = {
+        @annotation.tailrec
+        def loop(index : Int) : Int = {
+            if(index >= array.length) -1
+            else if(f(array(index))) index
+            else loop(index + 1)
+        }
+        loop(0)
     }
 
     def factorial(n : Int) : Int = {
