e960ca18cd057b81327cdb6e27bbae842d24e843
varun93 <1hvarun@gmail.com>
2019-01-07 15:18:52 -0600

    added List trait

---
+++ chapter-1/Chapter2.scala
@@ -0,0 +1,22 @@
+sealed trait List[+A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head : A, tail : List[A]) extends List[A] 
+
+object List{
+    def sum(ints : List[Int]) : Int = ints match {
+        case Nil => 0
+        case Cons(head,tail) => head + sum(tail)
+    }
+
+    def product(p : List[Double]) : Double = p match {
+        case Nil => 1.0
+        case Cons(0,_) => 0.0
+        case Cons(head,tail) => head * product(tail) 
+    }
+
+    def apply[A](as: A*): List[A] = {
+        if (as.isEmpty) Nil
+        else Cons(as.head, apply(as.tail: _*))
+    }
+
+}
